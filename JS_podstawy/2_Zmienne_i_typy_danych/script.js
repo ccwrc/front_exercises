95245942d22d8c6180a56d15d53cab82b1ecc791
ccwrc <j.muszelik@gmail.com>
2017-07-05 17:29:25 +0200

    zadanie 1 - done

--- JS_podstawy/2_Zmienne_i_typy_danych/script.js
+++ JS_podstawy/2_Zmienne_i_typy_danych/script.js
@@ -1,1 +1,27 @@
 
+// zadanie 1
+var number = 1;
+console.log(number); // 1
+console.log(typeof number); // number
+
+var string = "string";
+console.log(string); // string
+console.log(typeof string); // string
+
+var numberString = 2 + "dwa";
+console.log(numberString); // 2dwa
+console.log(typeof numberString); // string
+
+var boolean = true;
+console.log(boolean); // true
+console.log(typeof boolean); // boolean
+
+var special = null;
+console.log(special); // null
+console.log(typeof special); // object - w pewnym sensie błąd specyfikacji
+// https://stackoverflow.com/questions/18808226/why-is-typeof-null-object
+
+
+
+
+
