f6eb8984525df4cfd01816b04b98c8a76d6911d5
ccwrc <j.muszelik@gmail.com>
2017-07-05 17:06:36 +0200

    1_Pierwszy_skrypt - done

--- JS_podstawy/1_Pierwszy_skrypt/script.js
+++ JS_podstawy/1_Pierwszy_skrypt/script.js
@@ -1,8 +1,14 @@
 
 console.log("Twoje imię");
 // zadanie 1 - brak poprawnego wyświetlania PL znaków bez ustawionego utf-8
+// po ustawieniu kodowania poprawnie wyświetla komunikat w cudzysłowiu
 
 // zadanie 2 - JS nie nadaje się do operacji zmienno przecinkowych, wynik 10/3 to:
 // 3.3333333333333335 (Standard IEEE 754)
 // http://lidia-js.kis.p.lodz.pl/Systemy_Liczbowe/standard_754_teoria.php
+// generalnie operacje zwracają wynik
 
+// zadanie 3 
+// "Hello World"; -> "Hello World"
+// Hello World; -> Uncaught SyntaxError: Unexpected identifier
+// "Hello World; -> Uncaught SyntaxError: Invalid or unexpected token
