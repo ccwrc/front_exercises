abd814a03e0058a50ba7c41c0217841d9196f529
ccwrc <j.muszelik@gmail.com>
2017-07-05 16:59:37 +0200

    zadanie 2 - done

--- JS_podstawy/1_Pierwszy_skrypt/script.js
+++ JS_podstawy/1_Pierwszy_skrypt/script.js
@@ -2,3 +2,7 @@
 console.log("Twoje imię");
 // zadanie 1 - brak poprawnego wyświetlania PL znaków bez ustawionego utf-8
 
+// zadanie 2 - JS nie nadaje się do operacji zmienno przecinkowych, wynik 10/3 to:
+// 3.3333333333333335 (Standard IEEE 754)
+// http://lidia-js.kis.p.lodz.pl/Systemy_Liczbowe/standard_754_teoria.php
+
