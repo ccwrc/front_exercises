d4c4e99aaa671dc27741fc3d9e10530b05f3479e
ccwrc <j.muszelik@gmail.com>
2017-07-10 13:29:05 +0200

    zadanie 3 - done

--- JS_podstawy/3_Operatory/script.js
+++ JS_podstawy/3_Operatory/script.js
@@ -3,6 +3,9 @@ var var1 = 2;
 var var2 = true;
 console.log(var1 == var2);  // var1 = 1 true  // var1 = 2 false
   // luźne porównanie traktuje 1 jako prawdę
+var numberNumber = 10;
+var numberString = "10";
+console.log(numberNumber == numberString); // true
   
 
 // zadanie 2
@@ -11,3 +14,13 @@ var number2 = 4;
 var resultModulo = 0;
 resultModulo = number1 % number2;
 console.log(resultModulo);
+
+
+// zadanie 3
+var string1 = "dowolny1";
+var string2 = "dowolny2";
+var joinedStrings = "";
+joinedString = string1 + string2;
+console.log(joinedString);
+joinedString = string1 + " " + string2;
+console.log(joinedString);
