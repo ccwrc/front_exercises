2bbc39252d04bdb0b45141dfd9d2f3eb4a7534f3
ccwrc <j.muszelik@gmail.com>
2017-07-12 12:51:42 +0200

    zadanie 1 - done

---
+++ JS_podstawy/4_Kontrola_przeplywu/script.js
@@ -0,0 +1,15 @@
+//W pliku script.js stwórz dwie zmienne przechowujące liczby. Następnie za pomocą instrukcji 
+//warunkowej if ... else, wypisz w konsoli, 
+//        która z nich jest większa.
+
+// zadanie 1
+var number1 = -88;
+var number2 = 35;
+
+if (number1 > number2) {
+    console.log(number1 + " jest wieksza");
+} else if (number2 > number1) {
+    console.log(number2 + " jest wieksza");
+} else {
+    console.log("rowne lub brak mozliwosci porownania");
+}
