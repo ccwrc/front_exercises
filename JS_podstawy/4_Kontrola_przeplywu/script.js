db8d70585a83d28feec798350e900158e5b894e6
ccwrc <j.muszelik@gmail.com>
2017-07-28 14:27:42 +0200

    zadanie 6 - done

--- JS_podstawy/4_Kontrola_przeplywu/script.js
+++ JS_podstawy/4_Kontrola_przeplywu/script.js
@@ -52,6 +52,16 @@ function checkNumber(n) {
 }
 checkNumber(5);
 
+// zadanie 6
+for (var i = 1; i <= 3; i++) {
+    for (var j = 1; j <= 3; j++) {
+        console.log("i = " + i + ", j = " + j);
+    }
+}
+
+
+
+
 
 
 
