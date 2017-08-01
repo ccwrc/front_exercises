f4cd75f9fb1bed901589074193c98dc66a2c8fc1
ccwrc <j.muszelik@gmail.com>
2017-08-01 07:22:09 +0200

    zadanie 3 - done

--- JS_podstawy/5_Tablice/script.js
+++ JS_podstawy/5_Tablice/script.js
@@ -13,3 +13,38 @@ console.log(fruitsArray[fruitsArray.length - 1]);
 for (var i = 0; i < fruitsArray.length; i++) {
     console.log(fruitsArray[i]);
 }
+
+
+// zadanie 3 
+var freeNumbers = [11, 2, 2, 1, 1, 1, 1, 1, 1, 11];
+var sum = 0;
+for (var i = 0; i < freeNumbers.length; i++) {
+    sum += freeNumbers[i];
+}
+console.log("Suma: " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
